ff06b1859991645954799a49ce748a1eeed0ffe3
abzlrz <abuzolawrence@gmail.com>
2017-08-29 15:11:18 +0700

    some UI changes

---
+++ HRMS/Presentation/DialogBox/ApplicationForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.DialogBox
+{
+    public partial class ApplicationForm : Form
+    {
+        public ApplicationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Data you entered will be disregarded. Are you sure?",
+                   "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+            }
+        }
+    }
+}
---
+++ HRMS/Presentation/DialogBox/EmployeeExitForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.DialogBox
+{
+    public partial class EmployeeExitForm : Form
+    {
+        public EmployeeExitForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+                linkPath.Text = fullFilePath;
+        }
+        private void linkPathClick(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", "/select, \"" + fullFilePath + "\"");
+        }
+
+        private void openFileDialogFileOk(object sender, CancelEventArgs e)
+        {
+            this.fullFilePath = Path.Combine(Path.GetDirectoryName(openFileDialog.FileName), openFileDialog.FileName);
+            this.fileDirectory = Path.GetDirectoryName(openFileDialog.FileName);
+        }
+        private void EmployeeExitForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Data you entered will be disregarded. Are you sure?",
+                   "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+            }
+        }
+
+        private string fullFilePath;
+        private string fileDirectory;
+
+        
+    }
+}
